d8a58a11d85f20c65d16be50c9cec6d2a052213f
The_Bous <thebous1993@gmail.com>
2017-04-22 19:42:37 +0200

    adding navbar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { CoursesComponent } from './courses/courses.component';
 import { AuthorComponent } from './author/author.component';
 
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
