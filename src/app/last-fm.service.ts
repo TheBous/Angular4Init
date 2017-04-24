9d9a2d02e668f33438927947a79750d338449aaf
The_Bous <thebous1993@gmail.com>
2017-04-24 17:29:47 +0200

    finish similar songs & TODO

--- src/app/last-fm.service.ts
+++ src/app/last-fm.service.ts
@@ -4,12 +4,10 @@ import 'rxjs/add/operator/map';
 
 @Injectable()
 export class LastFmService {
-  constructor(
-    private http: Http
-  ) { }
+  constructor(private http: Http) { }
 
   getUser(artist) {
-    return this.http.get("http://ws.audioscrobbler.com/2.0/?method=artist.getsimilar&artist="+artist+"&api_key=823ebf7674a816efb2b538446d6ccf79&format=json")
+    return this.http.get("http://ws.audioscrobbler.com/2.0/?method=artist.getsimilar&artist="+artist+"&api_key=823ebf7674a816efb2b538446d6ccf79&format=json&limit=15")
       .map((res: Response) => res.json());
   }
 
